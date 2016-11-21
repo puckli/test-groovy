b89714955221b65325651f586aa9221be258800e
lwz <wenzhong.li@dmall.com>
2016-11-21 18:38:00 +0800

    init

---
+++ src/test/Test.groovy
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*
+
+class Test {
+	
+	@org.junit.Test
+	public void test() {
+		println "hello groovy"
+		
+	}
+	
+	@org.junit.Test
+	public void testBig() {
+		
+		
+	}
+}
---
+++ src/test/calculate/TestCal.groovy
@@ -0,0 +1,20 @@
+package test.calculate;
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class TestCal {
+
+	@Test
+	public void test() {
+		println 13.intdiv(5)
+	}
+	
+	@Test
+	public void testBig() {
+		BigDecimal bd = new BigDecimal(2);
+		
+		println bd.multiply(4).toString()
+	}
+}
